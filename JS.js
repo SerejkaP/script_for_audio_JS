1aa9296f5e5e101126461d65423e1457c22480b8
SerejkaP <haggardspark@gmail.com>
2021-03-26 23:27:29 +0600

    trying to take all informations about tracks

--- JS.js
+++ JS.js
@@ -1,5 +1,8 @@
-var time = document.getElementsByClassName("audio_row__duration audio_row__duration-s _audio_row__duration");
-if (time != null && time.length != 0) {
-var fullTime = time[0].innerText;
-alert("Продолжительность треков: " + fullTime);
-}
+var info = document.querySelectorAll(".audio_row__duration");
+var time=0;
+console.log(info);
+// if (time != null && time.length != 0) {
+// var fullTime = time[0].innerText;
+// alert("Продолжительность треков: " + fullTime);
+// }
+
