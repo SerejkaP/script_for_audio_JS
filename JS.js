1a4eb75c7af623ce15e8f604f05977f4b93a9f65
SerejkaP <haggardspark@gmail.com>
2021-03-27 15:36:09 +0600

    trying to make scrolling for script

--- JS.js
+++ JS.js
@@ -1,6 +1,13 @@
-var info = document.querySelectorAll(".audio_row__duration");
-var time=0;
-console.log(info);
+var info;
+var fir=0,sec=1;
+var times=0;
+while(fir!=sec){ 
+    fir=sec;
+    window.scrollTo(0, document.body.scrollHeight);
+    info = document.querySelectorAll(".audio_row__duration");
+    sec=info.length;
+}
+
 // if (time != null && time.length != 0) {
 // var fullTime = time[0].innerText;
 // alert("Продолжительность треков: " + fullTime);
