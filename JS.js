b0131a5bc47f22e5fa06421723485c71926ab375
SerejkaP <haggardspark@gmail.com>
2021-03-27 16:32:42 +0600

    Creating piececreating piece of script for converting seconds to time

--- JS.js
+++ JS.js
@@ -1,6 +1,7 @@
 var info;
 var a=0,b=1;
 var allSeconds=0;
+var seconds,minutes,hours,days,mounths;
 while(a!=b){ 
     a=b;
     window.scrollTo(0, document.body.scrollHeight);
@@ -15,7 +16,24 @@ for(let i=0;i<info.length;i++){
     if(time.length==3) allSeconds+=Number(time[2])+60*Number(time[1])+3600*Number(time[0]);
 }
 
+if(allSeconds>=60){
+    minutes = parseInt(allSeconds/60,10);
+    seconds = allSeconds-minutes*60;
+    if(minutes>=60){
+        hours = parseInt(minutes/60,10);
+        minutes = minutes-hours*60;
+        if(hours>=24){
+            days = parseInt(hours/24,10);
+            hours = hours-days*24;
+            if(days>=30){
+                mounths = parseInt(days/30);
+                days = days-months*30;
+            }
+        }
+    }
+}
 
+console.log('Время всех треков: '+mounths+' '+days+' '+hours+' '+minutes+' '+seconds);
 
 // if (time != null && time.length != 0) {
 // var fullTime = time[0].innerText;
