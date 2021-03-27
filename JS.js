21fabca2c3b40fd4d82b70da3efbfc5b43ab5dda
SerejkaP <haggardspark@gmail.com>
2021-03-27 16:11:12 +0600

    renamed variables

--- JS.js
+++ JS.js
@@ -1,19 +1,20 @@
 var info;
-var fir=0,sec=1;
-var times=0;
-while(fir!=sec){ 
-    fir=sec;
+var a=0,b=1;
+var allSeconds=0;
+while(a!=b){ 
+    a=b;
     window.scrollTo(0, document.body.scrollHeight);
     info = document.querySelectorAll(".audio_row__duration");
-    sec=info.length;
+    b=info.length;
 }
 
 for(let i=0;i<info.length;i++){
     let stringTime = info[i].innerText;
     let time = stringTime.split(":");
-    if(time.length==2) times+=Number(time[1])+60*Number(time[0]);
-    if(time.length==3) times+=Number(time[2])+60*Number(time[1])+3600*Number(time[0]);
+    if(time.length==2) allSeconds+=Number(time[1])+60*Number(time[0]);
+    if(time.length==3) allSeconds+=Number(time[2])+60*Number(time[1])+3600*Number(time[0]);
 }
+
 
 
 // if (time != null && time.length != 0) {
