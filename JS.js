cefb7362015cf7e444734d74564f9f946ae33eac
SerejkaP <haggardspark@gmail.com>
2021-03-28 17:52:05 +0600

    adding a check for script execution

--- JS.js
+++ JS.js
@@ -1,29 +1,30 @@
-var info;
+var info = document.querySelectorAll(".audio_row__duration"); //Получение всех записей;
 var a=0,b=1;
 var allSeconds=0;
 var seconds,minutes,hours,days,mounths;
 var stringTime="";
-var intervalId=setInterval(function() {
-    a=b;
-    window.scrollTo(0, document.body.scrollHeight); //Пролистывание страницы вниз
-    info = document.querySelectorAll(".audio_row__duration"); //Получение всех записей
-    b=info.length;
-    if(a==b){
-        clearInterval(intervalId);
+if (info != null && info.length != 0){
+    var intervalId=setInterval(function(){
+        a=b;
+        window.scrollTo(0, document.body.scrollHeight); //Пролистывание страницы вниз
+        info = document.querySelectorAll(".audio_row__duration"); //Получение всех записей
+        b=info.length;
+        if(a==b){
+            clearInterval(intervalId);
         
-        for(let i=0;i<info.length;i++){
-            let stringTime = info[i].innerText; //Получение текста с временем трека
-            let time = stringTime.split(":"); //Получение значений времени в массив
-            if(time.length==2) allSeconds+=Number(time[1])+60*Number(time[0]); //Получение секунд для случая без часов
-            if(time.length==3) allSeconds+=Number(time[2])+60*Number(time[1])+3600*Number(time[0]); //Получение секунд для случая с часами
-        }
+            for(let i=0;i<info.length;i++){
+                let stringTime = info[i].innerText; //Получение текста с временем трека
+                let time = stringTime.split(":"); //Получение значений времени в массив
+                if(time.length==2) allSeconds+=Number(time[1])+60*Number(time[0]); //Получение секунд для случая без часов
+                if(time.length==3) allSeconds+=Number(time[2])+60*Number(time[1])+3600*Number(time[0]); //Получение секунд для случая с часами
+            }
 
-        if(allSeconds>=60){
-            minutes = parseInt(allSeconds/60,10);
-            seconds = allSeconds-minutes*60; //Получение секунд
-            if(minutes>=60){
-                hours = parseInt(minutes/60,10);
-                minutes = minutes-hours*60; //Получение минут
+            if(allSeconds>=60){
+                minutes = parseInt(allSeconds/60,10);
+                seconds = allSeconds-minutes*60; //Получение секунд
+                if(minutes>=60){
+                    hours = parseInt(minutes/60,10);
+                    minutes = minutes-hours*60; //Получение минут
                     if(hours>=24){
                         days = parseInt(hours/24,10);
                         hours = hours-days*24; //Получение часов
@@ -35,12 +36,13 @@ var intervalId=setInterval(function() {
                 }
             }
 
-    stringTime+="Общая продолжительность треков: "
-    if(mounths!=undefined) stringTime+=mounths+" месяца, "; //Прокерки на наличие месяца, дня, часа
-    if(days!=undefined) stringTime+=days+" дня, ";
-    if(hours!=undefined) stringTime+=hours+" часа, ";
-    stringTime+=minutes+" минут, "+seconds+" секунды.";
-    console.log(stringTime);
-    window.scrollTo(0, 0); //Возврат на верх страницы
-    }
-}, 1000);
+            stringTime+="Общая продолжительность треков: "
+            if(mounths!=undefined) stringTime+=mounths+" месяца, "; //Прокерки на наличие месяца, дня, часа
+            if(days!=undefined) stringTime+=days+" дня, ";
+            if(hours!=undefined) stringTime+=hours+" часа, ";
+            stringTime+=minutes+" минут, "+seconds+" секунды.";
+            console.log(stringTime);
+            window.scrollTo(0, 0); //Возврат на верх страницы
+        }
+    }, 1000);
+}
